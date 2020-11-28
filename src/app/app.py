8a8973aac215f11c8d6e890597fe61ab5dc5cb7e
Roy Moran <romoran1@outlook.com>
2020-11-28 03:36:55 -0600

    add flask cors package and import

--- src/app/app.py
+++ src/app/app.py
@@ -2,12 +2,14 @@
 """Entry point for flask app."""
 import os
 from flask import Flask, Response
+from flask_cors import CORS
 from src.app.init_logger import InitLogger
 
 logger = InitLogger.instance('app', os.environ["APP_ENV"])
 logger.info('app started')
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/", methods=["GET"])
 def root():
